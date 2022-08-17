fa36bc8ffba01936224c870f65eeaa94070de9eb
bobby <bobby@debian-BULLSEYE-live-builder-AMD64>
2022-08-17 15:26:17 -0500

    3rd commit

--- initgit.sh
+++ initgit.sh
@@ -1,1 +1,1 @@
-git init; git add README.md; git commit -m first commit; git remote add origin git@github.com:robjohncolson/dotfiles.git; git push -u origin master
+gh repo create dotfiles --public; git init; git add README.md; git commit -m first commit; git remote add origin git@github.com:robjohncolson/dotfiles.git; git push -u origin master
